422204574de324bc4c8cde0e5ff1f0200eec9ebd
Trip Dacus <trip@oklo.com>
2023-09-22 16:49:24 -0400

    Add run all workcenter button

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from dash import Dash, Input, Output, callback, dcc, html
 
 # Statistical fluctuation size
 low_roll = 1
-high_roll = 6
-avg_roll = np.average([low_roll, high_roll])
+high_roll = 7
+avg_roll = 3.5
+# avg_roll = np.average([low_roll, (high_roll - 1)])
 
 # Workcenter Data
 workcenters = []
@@ -171,7 +172,7 @@ app.layout = dbc.Container(
                     className="text-primary text-center fs-3",
                 ),
                 html.Div(
-                    f"Each work center can do between {low_roll} and {high_roll} units of work.\n "
+                    f"Each work center can do between {low_roll} and {6} units of work.\n "
                     "However, work can only be passed down the line if there is enough inventory in the previous work center.\n"
                     "This is a 'perfectly balanced' system. Line balancing would tell us that the average throughput should be 3.5 units per cycle.\n"
                     "Is this what happens?\n",
@@ -184,15 +185,15 @@ app.layout = dbc.Container(
             className="text-center",
         ),
         dbc.Row(button_row),
-        # html.Div(
-        #     dbc.Button(
-        #         "Run all workcenters",
-        #         color="info",
-        #         className="me-1",
-        #         id="iterate_button",
-        #     ),
-        #     className="text-center",
-        # ),
+        html.Div(
+            dbc.Button(
+                "Run all workcenters",
+                color="info",
+                className="me-1",
+                id="run_all",
+            ),
+            className="text-center",
+        ),
         html.Div(
             dbc.Accordion(
                 [
@@ -204,6 +205,33 @@ app.layout = dbc.Container(
         ),
     ]
 )
+
+
+def do_work(
+    workcenter, previous_workcenter, performance, performance_avg, quota_offset
+):
+    """Do work on a workcenter and update the values."""
+    new_roll = np.random.randint(low_roll, high_roll)
+    if not isinstance(previous_workcenter, pd.DataFrame):
+        work_done = new_roll
+    else:
+        # Only can pull inventory from previous workcenter if there is enough inventory there
+        if new_roll <= previous_workcenter["inventory"][0]:
+            work_done = new_roll
+        else:
+            work_done = previous_workcenter["inventory"][0]
+
+        previous_workcenter["inventory"] -= work_done
+
+    workcenter["inventory"] += work_done
+    performance.append(work_done)
+    performance_avg.append(np.average(performance))
+
+    # The quotas are cummulative, so add the offset to the old value
+    old_quota_value = quota_offset[-1]
+    quota_offset.append(old_quota_value + (performance[-1] - avg_roll))
+
+    return new_roll, work_done
 
 
 @callback(
@@ -220,21 +248,16 @@ def dowork0(n_clicks):
     """Do some work at workcenter 0."""
     new_roll = None
     if n_clicks != 0:
-        new_roll = np.random.randint(low_roll, high_roll)
-        workcenter0["inventory"] += new_roll
-
-    performance0.append(new_roll)
-    performance0_avg.append(np.average(performance0))
-
-    # The quotas are cummulative, so add the offset to the old value
-    old_quota_value = quota_offset0[-1]
-    quota_offset0.append(old_quota_value + (performance0[-1] - avg_roll))
-
+        new_roll, work_done = do_work(
+            workcenter0, None, performance0, performance0_avg, quota_offset0
+        )
     return (
         dcc.Graph(
             figure=workcenter_fig(workcenter0),
         ),
-        f"Rolled {new_roll} unit(s) of work" if new_roll is not None else None,
+        f"Rolled {new_roll}, did {work_done} unit(s) of work"
+        if new_roll is not None
+        else None,
         dcc.Graph(figure=workcenter_stats(performance0_avg)),
         dcc.Graph(figure=quota_fig(quota_offset0)),
         True,
@@ -258,28 +281,17 @@ def dowork1(n_clicks):
     """Do some work at workcenter 1."""
     new_roll = None
     if n_clicks != 0:
-        new_roll = np.random.randint(low_roll, high_roll)
-
-        # Only can pull inventory from workcenter 1 if there is enough inventory there
-        if new_roll <= workcenter0["inventory"][0]:
-            transfer = new_roll
-        else:
-            transfer = workcenter0["inventory"][0]
-
-        workcenter1["inventory"] += transfer
-        workcenter0["inventory"] -= transfer
-        performance1.append(transfer)
-        performance1_avg.append(np.average(performance1))
-
-        # The quotas are cummulative, so add the offset to the old value
-        old_quota_value = quota_offset1[-1]
-        quota_offset1.append(old_quota_value + (performance1[-1] - avg_roll))
+        new_roll, work_done = do_work(
+            workcenter1, workcenter0, performance1, performance1_avg, quota_offset1
+        )
 
     return (
         dcc.Graph(figure=workcenter_fig(workcenter0)),
         dcc.Graph(figure=workcenter_fig(workcenter1)),
-        f"Rolled {new_roll} unit(s) of work" if new_roll is not None else None,
-        f"Transfered {transfer} -->" if new_roll is not None else None,
+        f"Rolled {new_roll}, did {work_done} unit(s) of work"
+        if new_roll is not None
+        else None,
+        f"Transfered {work_done} -->" if new_roll is not None else None,
         dcc.Graph(figure=workcenter_stats(performance1_avg)),
         dcc.Graph(figure=quota_fig(quota_offset1)),
         True,
@@ -303,28 +315,16 @@ def dowork2(n_clicks):
     """Do some work in workcenter 2."""
     new_roll = None
     if n_clicks != 0:
-        new_roll = np.random.randint(low_roll, high_roll)
-
-        # Only can pull inventory from workcenter 1 if there is enough inventory there
-        if new_roll <= workcenter1["inventory"][0]:
-            transfer = new_roll
-        else:
-            transfer = workcenter1["inventory"][0]
-
-        workcenter2["inventory"] += transfer
-        workcenter1["inventory"] -= transfer
-        performance2.append(transfer)
-        performance2_avg.append(np.average(performance2))
-
-        # The quotas are cummulative, so add the offset to the old value
-        old_quota_value = quota_offset2[-1]
-        quota_offset2.append(old_quota_value + (performance2[-1] - avg_roll))
-
+        new_roll, work_done = do_work(
+            workcenter2, workcenter1, performance2, performance2_avg, quota_offset2
+        )
     return (
         dcc.Graph(figure=workcenter_fig(workcenter1)),
         dcc.Graph(figure=workcenter_fig(workcenter2)),
-        f"Rolled {new_roll} unit(s) of work" if new_roll is not None else None,
-        f"Transfered {transfer} -->" if new_roll is not None else None,
+        f"Rolled {new_roll}, did {work_done} unit(s) of work"
+        if new_roll is not None
+        else None,
+        f"Transfered {work_done} -->" if new_roll is not None else None,
         dcc.Graph(figure=workcenter_stats(performance2_avg)),
         dcc.Graph(figure=quota_fig(quota_offset2)),
         True,
@@ -349,29 +349,18 @@ def dowork3(n_clicks):
     """Do some work at workcenter 3."""
     new_roll = None
     if n_clicks != 0:
-        new_roll = np.random.randint(low_roll, high_roll)
-
-        # Only can pull inventory from workcenter 1 if there is enough inventory there
-        if new_roll <= workcenter2["inventory"][0]:
-            transfer = new_roll
-        else:
-            transfer = workcenter2["inventory"][0]
-
-        workcenter3["inventory"] += transfer
-        workcenter2["inventory"] -= transfer
-        performance3.append(transfer)
-        performance3_avg.append(np.average(performance3))
-
-        # The quotas are cummulative, so add the offset to the old value
-        old_quota_value = quota_offset3[-1]
-        quota_offset3.append(old_quota_value + (performance3[-1] - avg_roll))
+        new_roll, work_done = do_work(
+            workcenter3, workcenter2, performance3, performance3_avg, quota_offset3
+        )
 
     return (
         dcc.Graph(figure=workcenter_fig(workcenter2)),
         dcc.Graph(figure=completed_fig(workcenter3)),
-        f"Rolled {new_roll} unit(s) of work" if new_roll is not None else None,
-        f"Transfered {transfer} -->" if new_roll is not None else None,
-        f"Completed {transfer} unit(s)!" if new_roll is not None else None,
+        f"Rolled {new_roll}, did {work_done} unit(s) of work"
+        if new_roll is not None
+        else None,
+        f"Transfered {work_done} -->" if new_roll is not None else None,
+        f"Completed {work_done} unit(s)!" if new_roll is not None else None,
         dcc.Graph(figure=completed_stats(performance3_avg)),
         dcc.Graph(figure=quota_fig(quota_offset3)),
         True,
@@ -459,19 +448,75 @@ def reset(n_clicks):
     )
 
 
-# @callback(
-#     Output("work_button0", "n_clicks"),
-#     Output("work_button1", "n_clicks"),
-#     Output("work_button2", "n_clicks"),
-#     Output("work_button3", "n_clicks"),
-#     Input("iterate_button", "n_clicks"),
-#     prevent_initial_call=True,
-# )
-# def iterate(n_clicks):
-#     """Iterate a multiple times through the workcenters."""
-#     if n_clicks != 0:
-#         return (1, 1, 1, 1)
-#     return (0, 0, 0, 0)
+@callback(
+    Output("workcenter0", "children", allow_duplicate=True),
+    Output("workcenter1", "children", allow_duplicate=True),
+    Output("workcenter2", "children", allow_duplicate=True),
+    Output("workcenter3", "children", allow_duplicate=True),
+    Output("performance_fig0", "children", allow_duplicate=True),
+    Output("performance_fig1", "children", allow_duplicate=True),
+    Output("performance_fig2", "children", allow_duplicate=True),
+    Output("performance_fig3", "children", allow_duplicate=True),
+    Output("quota_fig0", "children", allow_duplicate=True),
+    Output("quota_fig1", "children", allow_duplicate=True),
+    Output("quota_fig2", "children", allow_duplicate=True),
+    Output("quota_fig3", "children", allow_duplicate=True),
+    Output("work_value0", "children", allow_duplicate=True),
+    Output("work_value1", "children", allow_duplicate=True),
+    Output("work_value2", "children", allow_duplicate=True),
+    Output("work_value3", "children", allow_duplicate=True),
+    Output("transfer_value0", "children", allow_duplicate=True),
+    Output("transfer_value1", "children", allow_duplicate=True),
+    Output("transfer_value2", "children", allow_duplicate=True),
+    Output("transfer_value3", "children", allow_duplicate=True),
+    Output("work_button0", "disabled", allow_duplicate=True),
+    Output("work_button1", "disabled", allow_duplicate=True),
+    Output("work_button2", "disabled", allow_duplicate=True),
+    Output("work_button3", "disabled", allow_duplicate=True),
+    Input("run_all", "n_clicks"),
+    prevent_initial_call=True,
+)
+def run_all(n_clicks):
+    """Run all the workcenters at once"""
+    new_roll0, work_done0 = do_work(
+        workcenter0, None, performance0, performance0_avg, quota_offset0
+    )
+    new_roll1, work_done1 = do_work(
+        workcenter1, workcenter0, performance1, performance1_avg, quota_offset1
+    )
+    new_roll2, work_done2 = do_work(
+        workcenter2, workcenter1, performance2, performance2_avg, quota_offset2
+    )
+    new_roll3, work_done3 = do_work(
+        workcenter3, workcenter2, performance3, performance3_avg, quota_offset3
+    )
+
+    return (
+        dcc.Graph(figure=workcenter_fig(workcenter0)),
+        dcc.Graph(figure=workcenter_fig(workcenter1)),
+        dcc.Graph(figure=workcenter_fig(workcenter2)),
+        dcc.Graph(figure=completed_fig(workcenter3)),
+        dcc.Graph(figure=workcenter_stats(performance0_avg)),
+        dcc.Graph(figure=workcenter_stats(performance1_avg)),
+        dcc.Graph(figure=workcenter_stats(performance2_avg)),
+        dcc.Graph(figure=completed_stats(performance3_avg)),
+        dcc.Graph(figure=quota_fig(quota_offset0)),
+        dcc.Graph(figure=quota_fig(quota_offset1)),
+        dcc.Graph(figure=quota_fig(quota_offset2)),
+        dcc.Graph(figure=quota_fig(quota_offset3)),
+        f"Rolled {new_roll0},did {work_done0} unit(s) of work",
+        f"Rolled {new_roll1},did {work_done1} unit(s) of work",
+        f"Rolled {new_roll2},did {work_done2} unit(s) of work",
+        f"Rolled {new_roll3},did {work_done3} unit(s) of work",
+        f"Transfered {work_done1} -->",
+        f"Transfered {work_done2} -->",
+        f"Transfered {work_done3} -->",
+        f"Completed {work_done3} unit(s)!",
+        False,
+        True,
+        True,
+        True,
+    )
 
 
 # Run the app
